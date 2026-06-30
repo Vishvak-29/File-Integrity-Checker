7a9c1b1e0d65188391ca4760c5e3f45869c5e7b2
Vishvak-29 <25it118@rmkec.ac.in>
2026-06-30 20:06:17 +0530

    fileintegritychecker.py

--- fileintegritychecker.py
+++ fileintegritychecker.py
@@ -1,4 +1,15 @@
+#hash1
 import hashlib
-text="Vishvak@29"
-hash_value=hashlib.sha256(text.encode()).digest()
-print("HashValue ",hash_value)
+with open(input(),"rb") as p:
+    text1=p.read()
+hash_value1=hashlib.md5(text1).digest()
+#hash2
+with open(input(),"rb") as n:
+    text2=n.read()
+hash_value2=hashlib.md5(text2).digest()
+if(hash_value1==hash_value2):
+    print("The Both Files are same and data is untampered")
+else:
+    print("The Both Files are not same and data is tampered")
+print("hash_value1: ",hash_value1)
+print("hash_value2: ",hash_value2)
